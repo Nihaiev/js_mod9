b575fc40d3889373c2214daef6099ef18855e0c9
Ivan <nigaev.ivan@gmail.com>
2022-08-22 21:48:14 +0300

    setInterval

--- js/interval.js
+++ js/interval.js
@@ -6,10 +6,18 @@
 
 const logger = time => console.log(`Лог кожні ${time}ms - ${Date.now()}`);
 
-console.log('До виклику setInterval');
-setInterval(logger, 2000, 2000);
-console.log('Після виклику setInterval');
+// console.log('До виклику setInterval');
+// setInterval(logger, 2000, 2000);
+// console.log('Після виклику setInterval');
 
 /**
  * Щоб очистити інтервал у нас є clearInterval(intervalID)
  */
+
+const intervalID = setInterval(logger, 2000, 2000)
+const shouldCancelInterval = Math.random() > 0.3;
+console.log(shouldCancelInterval);
+
+if (shouldCancelInterval) {
+    clearInterval(intervalID)
+}
