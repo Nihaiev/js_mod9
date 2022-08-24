63ac918893336d115c76ea507abcc8dfb099ab50
Иван Нигаев <Nihaiev@it.group>
2022-08-24 14:41:07 +0300

    promise

---
+++ js/promise.js
@@ -0,0 +1,34 @@
+/**
+ * Створення проміса
+ * класс promise
+ * resolve
+ * reject
+ * Promise.prototype.then(onResolve, onReject)
+ */
+
+const promise = new Promise((resolve, reject) => { 
+    const canFulfill = Math.random() > 0.5;
+    setTimeout(() => {
+            if (canFulfill) {
+        resolve ('Проміс виконався успішно, з результатом')
+    }
+
+    reject('Проміс виконався з помилкою');
+    }, 2000)
+
+});
+
+promise.then(
+    result => {
+    console.log(`${result}`);
+},
+    error => {
+    console.log(`${error}`);
+})
+
+/**
+ * new Promise створює обіцянку яка записується у змінну promise
+ * обіцянка це просто обєкт
+ */
+
+//30
