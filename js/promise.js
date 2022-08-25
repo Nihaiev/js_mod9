23768d0751aa7b2d07993dcc492adea1d86c5a41
Иван Нигаев <Nihaiev@it.group>
2022-08-25 23:17:51 +0300

    Update promise.js

--- js/promise.js
+++ js/promise.js
@@ -6,25 +6,25 @@
  * Promise.prototype.then(onResolve, onReject)
  */
 
-const promise = new Promise((resolve, reject) => { 
-    const canFulfill = Math.random() > 0.5;
-    setTimeout(() => {
-            if (canFulfill) {
-        resolve ('Проміс виконався успішно, з результатом')
-    }
+// const promise = new Promise((resolve, reject) => {
+//     const canFulfill = Math.random() > 0.5;
+//     setTimeout(() => {
+//             if (canFulfill) {
+//         resolve ('Проміс виконався успішно, з результатом')
+//     }
 
-    reject('Проміс виконався з помилкою');
-    }, 2000)
+//     reject('Проміс виконався з помилкою');
+//     }, 2000)
 
-});
+// });
 
-promise.then(
-    result => {
-    console.log(`${result}`);
-},
-    error => {
-    console.log(`${error}`);
-})
+// promise.then(
+//     result => {
+//     console.log(`${result}`);
+// },
+//     error => {
+//     console.log(`${error}`);
+// })
 
 /**
  * new Promise створює обіцянку яка записується у змінну promise
@@ -32,3 +32,10 @@ promise.then(
  */
 
 //30
+
+/**
+ * Промісифікація функцій 
+ *  - Promise.resolve()
+ *  - Promise.reject()
+ */
+
